0f6f1863a4a60a5e3c791eb461aab16047a1d81d
scavicchio <salvy@nyu.edu>
2020-03-01 18:48:12 -0500

    utils.cpp

--- src/utils.cpp
+++ src/utils.cpp
@@ -393,7 +393,7 @@ void Utils::createModelFromFile(string path, float scale, vector<vec3> &vs, vect
     // Salvy is a BOSS // 
     if (endsWith(path, ".stl") || endsWith(path, ".STL")) {
         getline(file, header);
-
+        qDebug() << header;
         if (startsWith(trim(header), "solid")) {
             fileFormat = STL_ASCII;
         } else {
@@ -456,6 +456,7 @@ void Utils::createModelFromFile(string path, float scale, vector<Vec> &vs, vecto
             fileFormat = STL_BINARY;
         }
     }
+        qDebug() << header;
 
     // Parse based on format
     switch (fileFormat) {
