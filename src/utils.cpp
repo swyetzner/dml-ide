eb5149a127c4337f28bec587a4e57b071cc7a50b
scavicchio <salvy@nyu.edu>
2020-03-01 19:21:05 -0500

    utils.cpp

--- src/utils.cpp
+++ src/utils.cpp
@@ -414,7 +414,7 @@ void Utils::createModelFromFile(string path, float scale, vector<vec3> &vs, vect
         } 
         
         qDebug() << "New path: " << path; 
-        file.close() 
+        file.close();
     }
     // Salvy is a BOSS //
     ifstream file(path, ios::in | ios::binary);
@@ -495,7 +495,7 @@ void Utils::createModelFromFile(string path, float scale, vector<Vec> &vs, vecto
         if (endsWith(path,".STL")) { qDebug() << 4;  path.replace(path.end()-4,path.end(),".stl"); } 
         
         qDebug() << "New path: " << path;  
-        file.close()
+        file.close();
     }
     // Salvy is a BOSS //
     ifstream file(path, ios::in | ios::binary);
