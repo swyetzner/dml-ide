31f9438b49d55d22fa3d540153f6cfbfd72f9450
scavicchio <salvy@nyu.edu>
2020-03-01 19:18:45 -0500

    utils.cpp

--- src/utils.cpp
+++ src/utils.cpp
@@ -404,9 +404,15 @@ void Utils::createModelFromFile(string path, float scale, vector<vec3> &vs, vect
         qDebug() << "Attempting to correct file endings...";
         // this  is gonna check to see if the file exists with 
         // a different capitalization
-        if (endsWith(path,".stl")) { path.replace(path.end()-4,path.end(),".STL"); } 
+        if (endsWith(path,".stl")) { 
+            qDebug() << 2; 
+            path.replace(path.end()-4,path.end(),".STL"); 
+        } 
         
-        if (endsWith(path,".STL")) {  path.replace(path.end()-4,path.end(),".stl"); } 
+        if (endsWith(path,".STL")) {
+            qDebug() << 1;  
+            path.replace(path.end()-4,path.end(),".stl"); 
+        } 
         
         qDebug() << "New path: " << path;  
     }
@@ -416,6 +422,7 @@ void Utils::createModelFromFile(string path, float scale, vector<vec3> &vs, vect
             qDebug() << "File in path: " << path << " not found!";
             return;
     }
+
     // Salvy is a BOSS // 
     if (endsWith(path, ".stl") || endsWith(path, ".STL")) {
         getline(file, header);
@@ -483,9 +490,9 @@ void Utils::createModelFromFile(string path, float scale, vector<Vec> &vs, vecto
         qDebug() << "Attempting to correct file endings...";
         // this  is gonna check to see if the file exists with 
         // a different capitalization
-        if (endsWith(path,".stl")) { path.replace(path.end()-4,path.end(),".STL"); } 
+        if (endsWith(path,".stl")) { qDebug() << 3; path.replace(path.end()-4,path.end(),".STL"); } 
         
-        if (endsWith(path,".STL")) {  path.replace(path.end()-4,path.end(),".stl"); } 
+        if (endsWith(path,".STL")) { qDebug() << 4;  path.replace(path.end()-4,path.end(),".stl"); } 
         
         qDebug() << "New path: " << path;  
     }
