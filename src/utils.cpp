50b1a58883277723ed6fc8382b42b60df42f45b9
scavicchio <salvy@nyu.edu>
2020-03-01 19:13:43 -0500

    utils.cpp

--- src/utils.cpp
+++ src/utils.cpp
@@ -403,10 +403,10 @@ void Utils::createModelFromFile(string path, float scale, vector<vec3> &vs, vect
         qDebug() << "Attempting to correct file endings...";
         // this  is gonna check to see if the file exists with 
         // a different capitalization
-        if (endsWith(path,".stl")) { path.replace(path.end()-4,path.end(),".stl"); } 
+        if (endsWith(path,".stl")) { path.replace(path.end()-4,path.end(),".STL"); } 
         
         if (endsWith(path,".STL")) {  path.replace(path.end()-4,path.end(),".stl"); } 
-                qDebug() << "New path: " << path;
+        qDebug() << "New path: " << path;
 
         if (!file) {
             qDebug() << "File in path: " << path << " not found!";
@@ -480,7 +480,7 @@ void Utils::createModelFromFile(string path, float scale, vector<Vec> &vs, vecto
         qDebug() << "Attempting to correct file endings...";
         // this  is gonna check to see if the file exists with 
         // a different capitalization
-        if (endsWith(path,".stl")) { path.replace(path.end()-4,path.end(),".stl"); } 
+        if (endsWith(path,".stl")) { path.replace(path.end()-4,path.end(),".STL"); } 
         
         if (endsWith(path,".STL")) {  path.replace(path.end()-4,path.end(),".stl"); } 
         
